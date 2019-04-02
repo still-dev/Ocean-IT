7437b5fe47b728ac235eff0c87c18ee4e0aa06df
David <qkrgusdn159@naver.com>
2019-04-02 13:05:56 +0900

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/generic', function (req, res, next) {
+  res.render('generic');
+});
+
+router.get('/elements', function (req, res, next) {
+  res.render('elements');
+});
+module.exports = router;
