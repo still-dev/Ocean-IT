d078c6727ff7eac5881afd4378ec62d425f7d66e
David <qkrgusdn159@naver.com>
2019-05-29 16:47:07 +0900

    swfw

--- config/db.js
+++ config/db.js
@@ -1,11 +1,37 @@
 // 데이터베이스 기본 설정 파일
 var mysql = require('mysql');
-var db = mysql.createConnection({
+
+var db_config = {
     host     : 'localhost', // 외부에서 db 접속 시 localhost 가 아닌 서버pc의 ip 입력
     user     : 'root',
     password : 'ocean',
     database : 'ocean_it'
-});
+};
+var db = mysql.createConnection(db_config);
 
-db.connect(); //데이터베이스 연결
+handleDisconnect(db);
+
+function handleDisconnect(client){
+    client.on('error', function (error) {
+       if(!error.fatal) return;
+       if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+        console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+        db = mysql.createConnection(client.config);
+        handleDisconnect(db);
+        db.connect();
+    });
+}
+/*
+db.connect(function (err) {
+    if(err){
+        console.log(err);
+    }
+}); //데이터베이스 연결
+*/
+
 module.exports = db;
+
+// mysql timeout으로 발생하는 오류를 수정해야함
+// mysql pool 이나 에러 핸들링을 통해 해결요함
--- routes/research_fields.js
+++ routes/research_fields.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var db = require('../config/db');
 var router = express.Router();
-
+var research_fields_table;
+var rows;
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.redirect('research_fields/page/1');
+});
+
+router.get('/page/:num', function(req, res, next) {
+    /*db.query(`select * from research_fields`, function (error, fileds) {
+        if(error){
+            throw error;
+        }
+        research_fields_table = fields;
+        rows = fields.coun
+        res.render('index', {'research_fields_table': research_fields_table});
+    });*/
     res.render('research_fields');
 });
 
